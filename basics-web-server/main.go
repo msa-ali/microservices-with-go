a763c88794103d5c202e399025685d814f5e2bce
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2022-06-21 11:04:10 +0530

    custom server and gracefully shutting down server

--- basics-web-server/main.go
+++ basics-web-server/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/Altamashattari/microservices-with-go/basic-web-server/handlers"
 )
@@ -22,6 +25,29 @@ func main() {
 	// http.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request) {
 	// 	log.Println("Good Bye World")
 	// })
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+	// http.ListenAndServe(":9090", sm) // uses default serve mux - is an http handler
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
 
-	http.ListenAndServe(":9090", sm) // uses default serve mux - is an http handler
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	// gracefully shutdown
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
